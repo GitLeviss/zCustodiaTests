365f15c7c4539fbb0853276b3e3593a0cbb3d236
Levi Alves <alveslevi@icloud.com>
2025-12-01 16:20:28 -0300

    Fix - remove all files and folder

--- zCustodiaUi/locators/processing/ReceivablesElements.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace zCustodiaUi.locators.processing
-{
-    public class ReceivablesElements
-    {
-        public string ReceivablesPage { get; } = "//span[text()='Processamento Recebíveis']";
-        public string SecondCheckBox { get; } = "(//div[@class='mdc-checkbox__background'])[2]";
-        public string StatusPositionOnTheTable { get; } = "//app-status-column//div";
-    }
-}
